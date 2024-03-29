6b4c4d033c165669da165c55263dacaba61ed432
Einar Gednochsson <pete80pro.pb@gmail.com>
2024-03-29 12:22:24 +0100

    sm8150-common: drop presigned gpudriver update app

--- sm8150-common-vendor.mk
+++ sm8150-common-vendor.mk
@@ -758,7 +758,6 @@ PRODUCT_PACKAGES += \
     CneApp \
     IWlanService \
     TimeService \
-    com.qualcomm.qti.gpudrivers.msmnile.api30 \
     PowerOffAlarm \
     DeviceInfo \
     QtiTelephonyService \
